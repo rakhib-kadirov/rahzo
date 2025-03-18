8d183b0525b16d54a2ea3e19c726e11022dde95d
Rakhib Kadirov <RahiBihar@yandex.ru>
2025-03-18 23:29:38 +0300

    rahzo_18032025_2329

--- app/ui/message/message.tsx
+++ app/ui/message/message.tsx
@@ -32,7 +32,7 @@ interface Message {
 
 // extraHeaders: { origin: 'Access-Control-Allow-Origin' }
 // const socket: Socket = io("ws://localhost:3001/")
-const socket: Socket = io("wss://node.rahzo.ru:443", { path: "/socket.io/", timeout: 10000, reconnectionAttempts: 5, reconnectionDelay: 2000 })
+const socket: Socket = io("wss://node.rahzo.ru:443", { path: "/socket.io/", timeout: 100000 })
 // socket = io("http://26.137.137.103:3001")
 
 interface User {
