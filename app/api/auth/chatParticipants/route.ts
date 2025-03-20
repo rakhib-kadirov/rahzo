282850ce4fb2a39ba7efeef99f2ef28f2cc2dce8
Rakhib Kadirov <RahiBihar@yandex.ru>
2025-03-20 14:59:37 +0300

    rahzo_20032025_1459

--- app/api/auth/chatParticipants/route.ts
+++ app/api/auth/chatParticipants/route.ts
@@ -7,12 +7,12 @@ const prisma = new PrismaClient();
 export async function GET() {
     try {
         // 1️ Ищем существующий чат между пользователями
-        let chat = await prisma.chat.findMany({
+        let chats = await prisma.chat.findMany({
             include: { participants: { include: { user: { include: { message: true } } } } }
         });
-        console.log('CHATS_PART: ', chat)
+        console.log('CHATS_PART: ', chats)
 
-        return NextResponse.json({ chat: [chat] });
+        return NextResponse.json({ chats: [chats] });
     } catch (error) {
         console.error("Ошибка при получении чатов:", error);
         return NextResponse.json({ error: "Ошибка сервера" }, { status: 500 });
